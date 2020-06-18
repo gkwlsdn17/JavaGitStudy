2e132114c17fc38ca348ef703f5bad42388fb246
gkwlsdn17 <gkwlsdn17@gmail.com>
2020-06-18 14:33:15 +0900

    modify

--- chap13/WebContent/index.jsp
+++ chap13/WebContent/index.jsp
@@ -1,16 +1,73 @@
+<%@page import="java.util.List"%>
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ page import = "chap13.board.BoardDataBean" %>
+<%@ page import = "chap13.board.BoardDBBean" %>
+<%
+	BoardDBBean bdb = BoardDBBean.getInstance();
+	List<BoardDataBean>list = bdb.selectAll();
+	
+ %>
 <!DOCTYPE html>
 <html>
 <head>
 <meta charset="UTF-8">
 <title>Insert title here</title>
+
+<!-- jsp파일을 포함하는 문구, header.jsp에 부트스트랩 링크가 있다 -->
+<jsp:include page="header.jsp"></jsp:include>
+
 </head>
 <body>
-index.jsp
-<a href="deleteForm.jsp">글삭제</a>
-<a href="updateForm.jsp">글수정</a>
-<a href="view.jsp">글보기</a>
-<a href="writeForm.jsp">글작성</a>
+<div class="container">
+	<nav class="navbar navbar-expand-sm bg-light navbar-light">
+	  <ul class="navbar-nav">
+	    <li class="nav-item">
+	      <a class="nav-link" href="writeForm.jsp">글작성</a>
+	    </li>
+	    <li class="nav-item">
+	      <a class="nav-link" href="deleteForm.jsp">글삭제</a>
+	    </li>
+	    <li class="nav-item">
+	      <a class="nav-link" href="updateForm.jsp">글수정</a>
+	    </li>
+	  </ul>
+	</nav>
+	<div class="row">
+		<div class="col">
+			<table class="table table-bordered table-hover">
+			<tr>
+				<th>번호</th>
+				<th>작성자</th>
+				<th>이메일</th>
+				<th>제목</th>
+				<th>비밀번호</th>
+				<th>작성일자</th>
+				<th>조회수</th>
+				
+				<%
+					for(BoardDataBean temp: list){
+						out.print("<tr onclick ='row("+temp.getNum()+")'>");
+						out.print("<td>"+temp.getNum()+"</td>");
+						out.print("<td>"+temp.getWriter()+"</td>");
+						out.print("<td>"+temp.getEmail()+"</td>");
+						out.print("<td>"+temp.getSubject()+"</td>");
+						out.print("<td>"+temp.getPasswd()+"</td>");
+						out.print("<td>"+temp.getReg_date()+"</td>");
+						out.print("<td>"+temp.getReadcount()+"</td>");
+						out.println("</tr>");
+					}
+				%>
+			</table>
+			
+		</div>
+	</div>
+	
+</div>
 </body>
 </html>
+<script type="text/javascript">
+	function row(num){
+		location.href="view.jsp?num="+num;
+	}
+</script>
--- chap13/src/chap13/board/BoardDBBean.java
+++ chap13/src/chap13/board/BoardDBBean.java
@@ -3,6 +3,10 @@ package chap13.board;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class BoardDBBean {
 
@@ -65,5 +69,66 @@ public class BoardDBBean {
 	}
 	public void insertDetailArticle(String name, String subject, String email, String content, String passwd) {
 		
+	}
+	
+	public List<BoardDataBean> selectAll(){
+		List<BoardDataBean> list=new ArrayList<BoardDataBean>();
+		
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+		ResultSet rs = null;
+		try {
+			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+			conn = DriverManager.getConnection("jdbc:sqlserver://192.168.0.200:1433;databasename=khk_20200611", "sa", "8765432!");
+			pstmt = conn.prepareStatement("select * from board order by reg_date desc");
+			rs = pstmt.executeQuery();
+			
+			while(rs.next()) {
+				BoardDataBean bdb = new BoardDataBean();
+				bdb.setContent(rs.getString("content"));
+				bdb.setEmail(rs.getString("email"));
+				bdb.setIp(rs.getString("ip"));
+				bdb.setNum(rs.getInt("num"));
+				bdb.setPasswd(rs.getString("passwd"));
+				bdb.setRe_level(rs.getInt("re_level"));
+				bdb.setRe_step(rs.getInt("re_step"));
+				bdb.setReadcount(rs.getInt("readcount"));
+				bdb.setRef(rs.getInt("ref"));
+				bdb.setReg_date(rs.getTimestamp("reg_date"));
+				bdb.setSubject(rs.getString("subject"));
+				bdb.setWriter(rs.getString("writer"));
+				list.add(bdb);
+				
+			}
+			return list;
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		finally {
+			if(rs!=null)
+				try {
+					rs.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			if(pstmt!=null)
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			if(conn!=null)
+				try {
+					conn.close();
+				} catch (SQLException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			
+		}
+		return null;
 	}
 }
